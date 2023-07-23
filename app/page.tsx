280b32d9810227ec33d8eb9edb80e2b88999db2e
Wagner Andrade <wsawebmaster@yahoo.com.br>
2023-07-23 18:15:24 -0300

    Adicionado sessão Conhecimentos

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { HeroSection } from "./components/pages/home/hero-section";
+import { KnownTechs } from "./components/pages/home/known-techs";
 
 export default async function Home() {
   return <>
     <HeroSection />
+    <KnownTechs />
   </>;
 }
