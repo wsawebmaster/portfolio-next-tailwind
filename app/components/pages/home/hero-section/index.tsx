5113f7e07096c15d85cf75b21c7fd09cc4008444
Wagner Andrade <wsawebmaster@yahoo.com.br>
2023-07-24 08:08:29 -0300

    Adicionado sessão Contato com Hook Form e Zod

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -26,14 +26,17 @@ const MOCK_CONTACTS = [
 export const HeroSection = () => {
   return (
     <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
-      <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
+      <div className="container flex items-start justify-between flex-col-reverse lg:flex-row items-center text-center">
         <div className="w-full lg:max-w-[530px]">
           <p className="font-mono text-emerald-400">Olá, meu nome é</p>
           <h2 className="text-4xl font-medium mt-2">Wagner Andrade</h2>
 
-          <p className="text-gray-400 my-6 text-sm sm:text-base">Meu primeiro contato com tecnologia foi no ano de 1996 e desde então me apaixonei pela área...</p>
+          <p className="text-gray-400 my-6 text-sm sm:text-base">
+            Meu primeiro contato com tecnologia foi no ano de 1996 e desde então
+            me apaixonei pela área...
+          </p>
 
-          <div className="flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[340px]">
+          <div className="flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[340px] ">
             {Array.from({ length: 9 }).map((_, index) => (
               <TechBadge name="Next.js" />
             ))}
@@ -69,5 +72,5 @@ export const HeroSection = () => {
         />
       </div>
     </section>
-  )
+  );
 }
